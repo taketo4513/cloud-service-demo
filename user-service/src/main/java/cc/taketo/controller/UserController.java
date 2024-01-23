876491e0d849b8b4740a29aa261ccb88bc0de496
Zhang <taketo1015@foxmail.com>
2024-01-23 18:15:43 +0800

    Update: gateway validate

--- user-service/src/main/java/cc/taketo/controller/UserController.java
+++ user-service/src/main/java/cc/taketo/controller/UserController.java
@@ -3,10 +3,9 @@ package cc.taketo.controller;
 import cc.taketo.common.Result;
 import cc.taketo.entity.User;
 import cc.taketo.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -18,8 +17,11 @@ public class UserController {
     @Resource
     private UserService userService;
 
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("/get/{id}")
-    public Result<User> getUser(@PathVariable Integer id) {
+    public Result<User> getUser(@PathVariable Integer id, @RequestHeader(value = "X-Request", required = false) String request) {
+        logger.info(request);
         return Result.success(userService.getById(id));
     }
 
