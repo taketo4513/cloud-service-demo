9f6e31b43f09fa35740f9ad3cad5a01d2acf2e11
Zhang <taketo1015@foxmail.com>
2024-01-23 16:25:22 +0800

    Add: annotation

--- order-service/src/main/java/cc/taketo/Application.java
+++ order-service/src/main/java/cc/taketo/Application.java
@@ -6,9 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+// 启用服务发现
 @EnableDiscoveryClient
+// 启用Feign客户端 扫描指定包
 @EnableFeignClients(basePackages = "cc.taketo.client")
+// 扫描MyBatis Mapper接口
 @MapperScan("cc.taketo.mapper")
+// 标识Spring Boot应用
 @SpringBootApplication
 public class Application {
 
